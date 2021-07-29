380a5c1b1b53d80a9d37f8d907bbbc4b3129958d
mzawadi <mwaranguallan345@gmail.com>
2021-07-29 18:28:42 +0300

    implement express api

--- app.js
+++ app.js
@@ -38,4 +38,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use('/api', function(req, res, next){
+  var key = req.query['api-key'];
+
+  // key isn't present
+  if (!key) return next(error(400, 'api key required'));
+
+  // key is invalid
+  if (!~apiKeys.indexOf(key)) return next(error(401, 'invalid api key'));
+
+  // all good, store req.key for route access
+  req.key = key;
+  next();
+});
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+var apiKeys = ['foo', 'bar', 'baz'];
+
+var repos = [
+  { name: 'express' , url: 'https://github.com/expressjs/express' },
+  { name: 'stylus' , url: 'https://github.com/learnboost/stylus' },
+  { name: 'cluster' , url: 'https://github.com/learnboost/cluster' }
+];
+
+var users = [
+  { name: 'loki' },
+  { name: 'thor' },
+  { name: 'odin' }
+]
+
+var userRepos = {
+  loki: [repos[0], repos[1]],
+  thor: [repos[1]],
+  odin: [repos[2]]
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/ola', function (req, res, next) {
+  res.render('ola', { title: 'Ola Amigo'})
+})
+
+router.get('/api/users', function (req, res, next) {
+  console.log(users);
+  res.send(users);  
+})
+
+router.get('/api/repos', function (req, res, next) {
+  res.send(repos);  
+})
+
+router.get('/api/user/:name/repos', function(req, res, next){
+  var name = req.params.name;
+  var user = userRepos[name];
+
+  if (user) res.send(user);
+  else next();
 });
 
 module.exports = router;
